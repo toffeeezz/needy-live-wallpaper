693334704752d8e1bed2ff7802d6564c9091eebe
ttoaffers <animeone56@gmail.com>
2026-05-20 02:48:38 +0800

    layers must now be manually added to queue for rendering

--- app/src/main/java/com/example/needylivewallpaper/graphx/Renderer.kt
+++ app/src/main/java/com/example/needylivewallpaper/graphx/Renderer.kt
@@ -2,15 +2,27 @@ package com.example.needylivewallpaper.graphx
 
 import android.content.res.AssetManager
 import android.graphics.Canvas
+import com.example.needylivewallpaper.comp.LayerManager
+import com.example.needylivewallpaper.utils.Logger.logE
 
 object Renderer {
 
+    val renderQueue = arrayListOf<Layer>()
 
-    fun init(assets: AssetManager){
-
+    fun addQueue(layerManager: LayerManager){
+        for(layer in layerManager.layers) {
+            if (renderQueue.contains(layer)) {
+                logE("Layer is already in queue: $layer")
+                return
+            }
+            renderQueue.add(layer)
+        }
     }
 
     fun render(canvas: Canvas){
+        renderQueue.sortBy{it.zOrder}
 
+        for(layer in renderQueue)
+            layer.drawAllChildren(canvas)
     }
 }
