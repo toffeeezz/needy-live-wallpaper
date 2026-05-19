2cc30ee2ac031da0cb8cf22069ca36426903826f
ttoaffers <animeone56@gmail.com>
2026-05-20 05:15:27 +0800

    moved the logic from the container clas to this class

--- app/src/main/java/com/example/needylivewallpaper/graphx/Layer.kt
+++ app/src/main/java/com/example/needylivewallpaper/graphx/Layer.kt
@@ -1,8 +1,33 @@
 package com.example.needylivewallpaper.graphx
 
-import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.util.Size
+import com.example.needylivewallpaper.utils.Logger.logE
 
-open class Layer(val zOrder: Int) : Container() {
+abstract class Layer(var zOrder: Int) {
 
+    abstract var size: Size
+    val children: ArrayList<Node> = arrayListOf()
+    var isVisible = true
+
+    fun add(node: Node){
+        if(children.contains(node)){
+            logE("Adding a duplicate child: $node")
+            return
+        }
+        children.add(node)
+    }
+
+    fun remove(node: Node){
+        if(!children.contains(node)){
+            logE("This container does not contain a child: $node")
+            return
+        }
+        children.remove(node)
+    }
+
+    fun show(){isVisible = true}
+    fun hide(){isVisible = false}
+
+    abstract fun drawAllChildren(canvas: Canvas)
 }
