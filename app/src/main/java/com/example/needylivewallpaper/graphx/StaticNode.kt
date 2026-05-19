7c86d441413158de8a3a8b3ff0ec67860ee99d2f
ttoaffers <animeone56@gmail.com>
2026-05-20 05:21:07 +0800

    updated the constructor to match super

--- app/src/main/java/com/example/needylivewallpaper/graphx/StaticNode.kt
+++ app/src/main/java/com/example/needylivewallpaper/graphx/StaticNode.kt
@@ -2,5 +2,5 @@ package com.example.needylivewallpaper.graphx
 
 import android.graphics.Bitmap
 
-class StaticNode(bitmap: Bitmap) : Node(bitmap) {
+class StaticNode(layer: Layer, bitmap: Bitmap) : Node(layer, bitmap) {
 }
