c652278c245ac29e0c774935e7e01416594852ba
ttoaffers <animeone56@gmail.com>
2026-05-22 07:32:20 +0800

    update(render): Fixed a bug where if a duplicate layer is being added, it stops the loops instead of skipping it

--- app/src/main/java/com/example/needylivewallpaper/graphx/render/Renderer.kt
+++ app/src/main/java/com/example/needylivewallpaper/graphx/render/Renderer.kt
@@ -1,7 +1,11 @@
 package com.example.needylivewallpaper.graphx.render
 
 import android.graphics.Canvas
+import android.util.Log
+import com.example.needylivewallpaper.utils.Logger
+import com.example.needylivewallpaper.utils.Logger.logD
 import com.example.needylivewallpaper.utils.Logger.logE
+import com.example.needylivewallpaper.utils.Logger.logI
 
 object Renderer {
 
@@ -11,13 +15,15 @@ object Renderer {
         for(layer in layerManager.layers) {
             if (renderQueue.contains(layer)) {
                 logE("Layer is already in queue: $layer")
-                return
+                continue
             }
+            Log.d("Renderer","Added ${layer.javaClass.simpleName}")
             renderQueue.add(layer)
         }
     }
 
     fun render(canvas: Canvas){
+        Logger.logD("Drawing")
         renderQueue.sortBy{it.zOrder}
 
         for(layer in renderQueue)
