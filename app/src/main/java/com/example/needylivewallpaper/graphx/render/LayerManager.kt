e52fc6ec28fe192ffd6146da9400466b621c1361
ttoaffers <animeone56@gmail.com>
2026-05-22 07:29:02 +0800

    update(render): changed the class into an interface and removed redundant methods

--- app/src/main/java/com/example/needylivewallpaper/graphx/render/LayerManager.kt
+++ app/src/main/java/com/example/needylivewallpaper/graphx/render/LayerManager.kt
@@ -2,27 +2,8 @@ package com.example.needylivewallpaper.graphx.render
 
 import com.example.needylivewallpaper.utils.Logger.logE
 
-abstract class LayerManager {
-    val layers = arrayListOf<Layer>()
-
-
-    fun addLayer(layer: Layer){
-        if (layers.contains(layer)){
-            logE("Adding a duplicate layer: $layer")
-            return
-        }
-
-        layers.add(layer)
-    }
-
-    fun removeLayer(layer: Layer) {
-        if (!layers.contains(layer)) {
-            logE("This manager does not contain a layer: $layer")
-            return
-        }
-
-        layers.remove(layer)
-    }
+interface LayerManager {
+    val layers: ArrayList<Layer>
 
     fun show(){
         for(layer in layers)
