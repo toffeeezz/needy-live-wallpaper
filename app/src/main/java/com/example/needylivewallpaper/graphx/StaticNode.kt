c2e1aa0db61766330dc9cc242f4d2999eee8a2d9
ttoaffers <animeone56@gmail.com>
2026-05-22 00:28:16 +0800

    update(graphx): Update the imports after Layer class was moved to the render package

--- app/src/main/java/com/example/needylivewallpaper/graphx/StaticNode.kt
+++ app/src/main/java/com/example/needylivewallpaper/graphx/StaticNode.kt
@@ -1,6 +1,8 @@
 package com.example.needylivewallpaper.graphx
 
 import android.graphics.Bitmap
+import com.example.needylivewallpaper.graphx.render.Layer
 
 class StaticNode(layer: Layer, bitmap: Bitmap) : Node(layer, bitmap) {
+
 }
